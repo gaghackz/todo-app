424c35626bcaf01116f8e038bc2bd783d821a163
gaghackz <karanamgagan@gmail.com>
2025-04-21 15:01:48 +0530

    added cors exception

--- backend/index.ts
+++ backend/index.ts
@@ -1,27 +1,22 @@
-import express from "express"
+import express from "express";
 import cors from "cors";
-import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import { userRouter } from "./routes/user_routes";
 import { todoRouter } from "./routes/todo_routes";
 
-
-
-
-
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 const corsOption = {
-    origin: ['http://localhost:5173'],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-}
+  origin: ["http://localhost:5173", "http://localhost:5173"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
 app.use(cors(corsOption));
 
-app.use("/api/v1", userRouter)
-app.use("/api/v1",todoRouter)
+app.use("/api/v1", userRouter);
+app.use("/api/v1", todoRouter);
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-}
-)
+  console.log("Server is running on port 3000");
+});
